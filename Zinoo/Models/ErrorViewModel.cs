090ca43ddba64c37864f092876046ff7e009e321
Vahid Aria <Vahid@ARIA-LAPTOP>
2022-09-11 16:18:56 +0430

    remove web project

--- Zinoo/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Zinoo.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
